7ffa181ab8f8741eff2dc43041332df1c3cc6870
heumsi <heumsi@naver.com>
2021-01-29 21:00:34 +0900

    refactor: health check 용 도메인 별도로 분리

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
-from routers import router
+from health.external_interface.routers import router as health_routers
 
 app = FastAPI()
-app.include_router(router)
+app.include_router(health_routers)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
