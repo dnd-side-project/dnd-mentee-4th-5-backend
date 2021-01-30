50e30c4256bb16f1c01f1f91832278f3bf02fceb
heumsi <heumsi@naver.com>
2021-01-30 16:08:16 +0900

    refactor: test_routers 에서 사용하지 않는 코드 삭제

--- tests/users/external_interface/test_routers.py
+++ tests/users/external_interface/test_routers.py
@@ -1,13 +1,3 @@
-import pytest
-
-from users.infra_structure.in_memory_repository import InMemoryUserRepository
-
-
-@pytest.fixture
-def user_repository():
-    return InMemoryUserRepository()
-
-
 def test_post_users(client):
     response = client.post("/users", json={"user_id": "heumsi", "user_name": "heumsi", "hashed_password": "1234"})
     assert response.status_code == 200
