c9a7317cc0f3ccc8c1072cf089e48894026047b2
mayjunjuly0313 <tank6138@gmail.com>
2021-01-31 23:06:04 +0900

    feat: external interface layer 추가

---
+++ app/reviews/external_interface/routers.py
@@ -0,0 +1,33 @@
+import uuid
+
+from dependency_injector.wiring import Provide, inject
+from fastapi import APIRouter, Depends
+from reviews.application.dtos import CreateReviewInputDto
+from reviews.application.service import ReviewApplicationService
+from reviews.domain.repository import ReviewRepository
+from reviews.domain.value_objects import ReviewRating
+from reviews.infra_structure.container import Container
+from users.domain.value_objects import UserId
+
+router = APIRouter(
+    prefix="/users/reviews",
+    tags=["reviews"],
+)
+
+
+@router.post("")
+@inject
+def create_review(
+        request: CreateReviewJsonRequest,
+        review_repository: ReviewRepository = Depends(Provide[Container.review_repository])
+):
+    review_application_service = ReviewApplicationService(review_repository=review_repository)
+    input_dto = CreateReviewInputDto(
+        review_id=uuid.uuid4(),
+        drink_id=request.drink_id,
+        user_id=UserId(value=request.user_id),
+        rating=ReviewRating(value=request.rating),
+        comment=request.comment,
+        created_at=request.created_at
+    )
+    review_application_service.create_review(input_dto=input_dto)
