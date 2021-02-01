5daf445844d536fa082ed237465f4ad0ba384f5e
heumsi <heumsi@naver.com>
2021-02-02 02:04:25 +0900

    feat: Shared kernel에 FailedJsonResponse 추가 (#9)

--- app/users/application/exceptions.py
+++
@@ -1,8 +0,0 @@
-class UserAlreadyExistError(Exception):
-    def __init__(self, msg: str) -> None:
-        self.msg = msg
-
-
-class UserNotExistError(Exception):
-    def __init__(self, msg: str) -> None:
-        self.msg = msg
