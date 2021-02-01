f50440d8ffee20fdf62205e096dc21bc11222098
heumsi <heumsi@naver.com>
2021-02-02 00:29:16 +0900

    Shared kernel 도메인에 SuccessOutputDto 및 __bool__ 메서드 추가 수정 (#8)
    * refactor: SuccessOutputDto 및 __bool__ 메서드 추가
    
    * refactor: SuccessOutputDto 에 status 삭제
    
    * refactor: FailedOutputDto 에서 status -> type 으로 이름 변경

--- app/shared_kernel/application/dtos.py
+++ app/shared_kernel/application/dtos.py
@@ -4,30 +4,34 @@ from typing import ClassVar
 from pydantic import BaseModel
 
 
-class OutputDto(BaseModel, abc.ABC):
-    status: str
+class SuccessOutputDto(BaseModel, abc.ABC):
+    def __bool__(self) -> bool:
+        return True
 
 
-class FailedOutputDto(OutputDto):
+class FailedOutputDto(BaseModel):
     RESOURCE_ERROR: ClassVar = "ResourceError"
     PARAMETERS_ERROR: ClassVar = "ParametersError"
     SYSTEM_ERROR: ClassVar = "SystemError"
 
-    status: str
+    type: str
     message: str
 
     @classmethod
     def build_from_invalid_request_object(cls, message: str = ""):
-        return cls(status=cls.PARAMETERS_ERROR, message=message)
+        return cls(type=cls.PARAMETERS_ERROR, message=message)
 
     @classmethod
     def build_resource_error(cls, message: str = ""):
-        return cls(status=cls.RESOURCE_ERROR, message=message)
+        return cls(type=cls.RESOURCE_ERROR, message=message)
 
     @classmethod
     def build_system_error(cls, message: str = ""):
-        return cls(status=cls.SYSTEM_ERROR, message=message)
+        return cls(type=cls.SYSTEM_ERROR, message=message)
 
     @classmethod
     def build_parameters_error(cls, message: str = ""):
-        return cls(status=cls.PARAMETERS_ERROR, message=message)
+        return cls(type=cls.PARAMETERS_ERROR, message=message)
+
+    def __bool__(self) -> bool:
+        return False
