f16006ecab5b4cb0af373645589d0da8d28cbdf2
heumsi <heumsi@gmail.com>
2021-02-03 00:37:29 +0900

    fix: shared_kernel 에 있던 잘못된 테스트 수정

--- tests/shared_kernel/application/test_dtos.py
+++ tests/shared_kernel/application/test_dtos.py
@@ -5,19 +5,13 @@ def test_success_output_dto():
     class DerivedSuccessOutputDto(SuccessOutputDto):
         pass
 
-    actual = DerivedSuccessOutputDto()
-    if actual:
-        assert True
-    else:
-        assert False
+    actual = DerivedSuccessOutputDto().status
+    expected = True
+    assert actual == expected
 
 
 def test_failed_output_dto():
     actual = FailedOutputDto.build_from_invalid_request_object()
     expected = FailedOutputDto(type=FailedOutputDto.PARAMETERS_ERROR, message="")
 
-    if not actual:
-        assert True
-    else:
-        assert False
-    assert actual == expected
+    assert actual.status == expected.status
