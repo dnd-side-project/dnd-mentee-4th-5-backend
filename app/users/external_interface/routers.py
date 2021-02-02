5e2b02a6304e9d91d4515c9c693ea51028aa38ad
heumsi <heumsi@gmail.com>
2021-02-03 01:47:03 +0900

    refactor: users routers 에서 UserApplicationService 를 주입받도록 수정

--- app/users/external_interface/routers.py
+++ app/users/external_interface/routers.py
@@ -6,7 +6,6 @@ from shared_kernel.external_interface.json_dto import FailedJsonResponse
 from container import Container
 from users.application.dtos import CreateUserInputDto
 from users.application.service import UserApplicationService
-from users.domain.repository import UserRepository
 from users.external_interface.json_dto import CreateUserJsonRequest
 
 router = APIRouter(
@@ -19,9 +18,8 @@ router = APIRouter(
 @inject
 def create_user(
     request: CreateUserJsonRequest,
-    user_repository: UserRepository = Depends(Provide[Container.user_repository]),
+    user_application_service: UserApplicationService = Depends(Provide[Container.user_application_service]),
 ):
-    user_application_service = UserApplicationService(user_repository=user_repository)
     input_dto = CreateUserInputDto(
         user_id=request.user_id,
         user_name=request.user_name,
