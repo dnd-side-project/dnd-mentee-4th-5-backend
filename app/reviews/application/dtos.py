902ae22fa9f7d4fe47166e71cbf6a2581c4b3a87
mayjunjuly0313 <tank6138@gmail.com>
2021-02-07 15:09:17 +0900

    feat: application layer 추가

--- app/reviews/application/dtos.py
+++ app/reviews/application/dtos.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 from uuid import UUID
 
 from pydantic import BaseModel, Field
@@ -7,35 +8,50 @@ from users.domain.value_objects import UserId
 
 
 class FindReviewInputDto(BaseModel):
-    review_id: UUID
+    review_id: str
 
 
 class FindReviewOutputDto(BaseModel):
-    review_id: UUID
-    drink_id: UUID
-    user_id: UserId
-    rating: ReviewRating
+    review_id: str
+    drink_id: str
+    user_id: str
+    rating: int
     comment: str
     created_at: float
 
 
 class CreateReviewInputDto(BaseModel):
-    review_id: UUID
-    drink_id: UUID
-    user_id: UserId
-    rating: ReviewRating
+    drink_id: str
+    user_id: str
+    rating: int
     comment: str
     created_at: float
 
 
 class UpdateReviewInputDto(BaseModel):
-    review_id: UUID
-    drink_id: UUID
-    user_id: UserId
-    rating: ReviewRating
+    review_id: str
+    drink_id: str
+    user_id: str
+    rating: int
     comment: str
     created_at: float
 
 
 class DeleteReviewInputDto(BaseModel):
-    review_id: UUID
+    review_id: str
+
+
+class FindReviewsByUserIdInputDto(BaseModel):
+    user_id: str
+
+
+class FindReviewsByUserIdOutputDto(BaseModel):
+    reviews_dicts: List[dict]
+
+
+class FindReviewsByDrinkIdInputDto(BaseModel):
+    drink_id: str
+
+
+class FindReviewsByDrinkIdOutputDto(BaseModel):
+    reviews_dicts: List[dict]
--- app/reviews/application/service.py
+++ app/reviews/application/service.py
@@ -1,65 +1,134 @@
-from reviews.application.dtos import (
-    CreateReviewInputDto,
-    DeleteReviewInputDto,
-    FindReviewInputDto,
-    FindReviewOutputDto,
-    UpdateReviewInputDto,
-)
-from reviews.application.exceptions import ReviewAlreadyExistError, ReviewNotExistError
+import uuid
+from typing import Optional, Union
+
+from reviews.application.dtos import (CreateReviewInputDto,
+                                      DeleteReviewInputDto, FindReviewInputDto,
+                                      FindReviewOutputDto,
+                                      FindReviewsByDrinkIdInputDto,
+                                      FindReviewsByDrinkIdOutputDto,
+                                      FindReviewsByUserIdInputDto,
+                                      FindReviewsByUserIdOutputDto,
+                                      UpdateReviewInputDto)
 from reviews.domain.entities import Review
 from reviews.domain.repository import ReviewRepository
+from reviews.domain.value_objects import ReviewRating, UserId
+from shared_kernel.application.dtos import FailedOutputDto
 
 
 class ReviewApplicationService:
     def __init__(self, review_repository: ReviewRepository) -> None:
-        self._user_repository = review_repository
+        self._review_repository = review_repository
 
-    def find_review(self, input_dto: FindReviewInputDto):
-        review = self._user_repository.find_by_review_id(input_dto.review_id)
-        if review is None:
-            raise ReviewNotExistError(f"{str(input_dto.review_id)}의 리뷰를 찾을 수 없습니다.")
+    def find_review(self, input_dto: FindReviewInputDto) -> Union[FailedOutputDto, FindReviewOutputDto]:
+        try:
+            review_id = uuid.UUID(input_dto.review_id)
+            review = self._review_repository.find_by_review_id(review_id)
+            if review is None:
+                return FailedOutputDto.build_resource_error(
+                    message=f"{str(input_dto.review_id)}의 리뷰를 찾을 수 없습니다."
+                )
 
-        return FindReviewOutputDto(
-            review_id=review.id,
-            drink_id=review.drink_id,
-            user_id=review.user_id,
-            rating=review.rating,
-            comment=review.comment,
-            created_at=review.created_at,
-        )
+            return FindReviewOutputDto(
+                review_id=str(review.id),
+                drink_id=str(review.drink_id),
+                user_id=str(review.user_id),
+                rating=int(review.rating),
+                comment=review.comment,
+                created_at=review.created_at,
+            )
+        except Exception as e:
+            return FailedOutputDto.build_system_error(message=str(e))
 
-    def create_review(self, input_dto: CreateReviewInputDto) -> None:
-        if self._user_repository.find_by_review_id(input_dto.review_id) is not None:
-            raise ReviewAlreadyExistError(f"{str(input_dto.review_id)}의 리뷰가 이미 존재합니다.")
+    def create_review(self, input_dto: CreateReviewInputDto) -> Optional[FailedOutputDto]:
+        try:
+            drink_id = uuid.UUID(input_dto.drink_id)
+            user_id = UserId(value=input_dto.user_id)
+            if self._review_repository.find_by_drink_id_user_id(drink_id, user_id) is not None:
+                return FailedOutputDto.build_resource_error(
+                    f"user: {str(user_id)}, drink: {str(drink_id)} 의 리뷰가 이미 존재합니다."
+                )
 
-        review = Review(
-            id=input_dto.review_id,
-            drink_id=input_dto.drink_id,
-            user_id=input_dto.user_id,
-            rating=input_dto.rating,
-            comment=input_dto.comment,
-            created_at=input_dto.created_at,
-        )
-        self._user_repository.add(review)
+            review = Review(
+                id=uuid.uuid5(
+                    uuid.NAMESPACE_DNS, name=input_dto.user_id+input_dto.drink_id+str(input_dto.created_at)
+                ),
+                drink_id=drink_id,
+                user_id=user_id,
+                rating=ReviewRating(value=input_dto.rating),
+                comment=input_dto.comment,
+                created_at=input_dto.created_at
+            )
+            self._review_repository.add(review)
+        except Exception as e:
+            return FailedOutputDto.build_system_error(message=str(e))
 
-    def update_review(self, input_dto: UpdateReviewInputDto) -> None:
-        review = self._user_repository.find_by_review_id(input_dto.review_id)
-        if review is None:
-            raise ReviewNotExistError(f"{str(input_dto.review_id)}의 리뷰를 찾을 수 없습니다.")
+    def update_review(self, input_dto: UpdateReviewInputDto) -> Optional[FailedOutputDto]:
+        try:
+            review_id = uuid.UUID(input_dto.review_id)
+            review = self._review_repository.find_by_review_id(review_id)
+            if review is None:
+                return FailedOutputDto.build_resource_error(
+                    f"{str(input_dto.review_id)}의 리뷰를 찾을 수 없습니다."
+                )
 
-        review = Review(
-            id=input_dto.review_id,
-            drink_id=input_dto.drink_id,
-            user_id=input_dto.user_id,
-            rating=input_dto.rating,
-            comment=input_dto.comment,
-            created_at=input_dto.created_at,
-        )
-        self._user_repository.update(review)
+            review = Review(
+                id=uuid.UUID(input_dto.review_id),
+                drink_id=uuid.UUID(input_dto.drink_id),
+                user_id=UserId(value=input_dto.user_id),
+                rating=ReviewRating(value=input_dto.rating),
+                comment=input_dto.comment,
+                created_at=input_dto.created_at
+            )
+            self._review_repository.update(review)
+        except Exception as e:
+            return FailedOutputDto.build_system_error(message=str(e))
 
-    def delete_review(self, input_dto: DeleteReviewInputDto) -> None:
-        review = self._user_repository.find_by_review_id(input_dto.review_id)
-        if review is None:
-            raise ReviewNotExistError(f"{str(input_dto.review_id)}의 리뷰를 찾을 수 없습니다.")
+    def delete_review(self, input_dto: DeleteReviewInputDto) -> Optional[FailedOutputDto]:
+        try:
+            review_id = uuid.UUID(input_dto.review_id)
+            review = self._review_repository.find_by_review_id(review_id)
+            if review is None:
+                return FailedOutputDto.build_resource_error(
+                    f"{str(input_dto.review_id)}의 리뷰를 찾을 수 없습니다."
+                )
 
-        self._user_repository.delete_by_review_id(input_dto.review_id)
+            self._review_repository.delete_by_review_id(review_id)
+        except Exception as e:
+            return FailedOutputDto.build_system_error(message=str(e))
+
+    def find_reviews_by_user_id(
+            self, input_dto: FindReviewsByUserIdInputDto
+    ) -> Union[FailedOutputDto, FindReviewsByUserIdOutputDto]:
+        try:
+            user_id = UserId(value=input_dto.user_id)
+            reviews = self._review_repository.find_all_by_user_id(user_id)
+            reviews_dicts = [review.dict() for review in reviews]
+            return FindReviewsByUserIdOutputDto(reviews_dicts=reviews_dicts)
+
+        except Exception as e:
+            return FailedOutputDto.build_system_error(message=str(e))
+
+    def find_reviews_by_drink_id(self, input_dto: FindReviewsByDrinkIdInputDto) \
+            -> Union[FailedOutputDto, FindReviewsByDrinkIdOutputDto]:
+        try:
+            drink_id = uuid.UUID(input_dto.drink_id)  
+            reviews = self._review_repository.find_all_by_drink_id(drink_id)
+            reviews_dicts = [review.dict() for review in reviews]
+            return FindReviewsByDrinkIdOutputDto(reviews_dicts=reviews_dicts)
+      
+        except Exception as e:
+            return FailedOutputDto.build_system_error(message=str(e))  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
