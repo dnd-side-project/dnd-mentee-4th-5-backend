79655460cbdf2ce70d5ca5df480b95e4e0925e80
mayjunjuly0313 <tank6138@gmail.com>
2021-02-07 16:52:25 +0900

    feat: VO 에 filtering Enums 추가

--- app/drinks/domain/repository.py
+++ app/drinks/domain/repository.py
@@ -3,13 +3,18 @@ from typing import List, Optional
 from uuid import UUID
 
 from drinks.domain.entities import Drink
-from drinks.domain.value_objects import OrderType
+from drinks.domain.value_objects import DrinkType, FilterType, OrderType
 
 
 class DrinkRepository(metaclass=ABCMeta):
+    # test use only
     @abstractmethod
-    def find_all(self, order: OrderType = OrderType.REVIEW_NUM_DESC) -> List[Drink]:
+    def find_all(self) -> List[Drink]:
         pass
+
+    # @abstractmethod
+    # def find_all(self, drink_type: DrinkType, filter_type: FilterType, order: OrderType) -> List[Drink]:
+    #     pass
 
     @abstractmethod
     def find_by_drink_id(self, drink_id: UUID) -> Optional[Drink]:
--- app/drinks/domain/value_objects.py
+++ app/drinks/domain/value_objects.py
@@ -5,14 +5,30 @@ from pydantic import BaseModel, Field
 
 
 class DrinkRating(BaseModel):
-    MIN_VALUE: ClassVar[int] = 0
-    MAX_VALUE: ClassVar[int] = 5
-    __root__: float = Field(alias="value", default=0, ge=MIN_VALUE, le=MAX_VALUE)
+    MIN_VALUE: ClassVar[int] = 0.0
+    MAX_VALUE: ClassVar[int] = 5.0
+    __root__: float = Field(alias="value", default=0.0, ge=MIN_VALUE, le=MAX_VALUE)
 
     def __float__(self) -> float:
         return self.__root__
 
 
-class OrderType(Enum):
-    REVIEW_NUM_DESC = "reviews_num_desc"
-    REVIEW_NUM_ASC = "reviews_num_asc"
+class DrinkType(str, Enum):
+    ALL = "ALL"
+    BEER = "BEER"
+    WINE = "WINE"
+    LIQUOR = "LIQUOR"
+    SAKE = "SAKE"
+    SOJU = "SOJU"
+    ETC = "ETC"
+
+
+class FilterType(str, Enum):
+    RATING = "RATING"
+    REVIEW = "REVIEW"
+    WISH = "WISH"
+
+
+class OrderType(str, Enum):
+    DESC = "DESC"
+    ASC = "ASC"
