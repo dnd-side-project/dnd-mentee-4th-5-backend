30beefafa5a005b5cc9e7ab7a26a4b78846b6a54
mayjunjuly0313 <tank6138@gmail.com>
2021-02-07 15:08:30 +0900

    feat: domain layer 추가

--- app/reviews/domain/entities.py
+++ app/reviews/domain/entities.py
@@ -1,14 +1,17 @@
+from typing import ClassVar
+
 from pydantic import BaseModel, Field
 from pydantic.types import UUID
-from reviews.domain.value_objects import ReviewRating
-from users.domain.value_objects import UserId
+from reviews.domain.value_objects import ReviewRating, UserId
 
 
 class Review(BaseModel):
     # uuid5 for test purpose. Use uuid4 later
+    MIN_COMMENT_LEN: ClassVar[int] = 0
+    MAX_COMMENT_LEN: ClassVar[int] = 300
     id: UUID
     drink_id: UUID
     user_id: UserId
     rating: ReviewRating
-    comment: str = Field(min_length=0, max_length=300)
-    created_at: float = Field(ge=0)
+    comment: str = Field(min_length=MIN_COMMENT_LEN, max_length=MAX_COMMENT_LEN)
+    created_at: float
--- app/reviews/domain/repository.py
+++ app/reviews/domain/repository.py
@@ -3,15 +3,28 @@ from typing import List, Optional
 from uuid import UUID
 
 from reviews.domain.entities import Review
+from reviews.domain.value_objects import OrderType, UserId
 
 
 class ReviewRepository(metaclass=ABCMeta):
     @abstractmethod
-    def find_all(self) -> List[Review]:
+    def find_all(self, order: OrderType = OrderType.LIKE_DESC) -> List[Review]:
+        pass
+
+    @abstractmethod
+    def find_all_by_user_id(self, user_id: UserId, order: OrderType = OrderType.LIKE_DESC) -> List[Review]:
+        pass
+
+    @abstractmethod
+    def find_all_by_drink_id(self, drink_id: UUID, order: OrderType = OrderType.LIKE_DESC) -> List[Review]:
         pass
 
     @abstractmethod
     def find_by_review_id(self, review_id: UUID) -> Optional[Review]:
+        pass
+
+    @abstractmethod
+    def find_by_drink_id_user_id(self, drink_id: UUID, user_id: UserId) -> Optional[Review]:
         pass
 
     @abstractmethod
