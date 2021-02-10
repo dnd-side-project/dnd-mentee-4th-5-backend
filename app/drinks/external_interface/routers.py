7362d527298f41a631e707da8265e1a67544f988
mayjunjuly0313 <tank6138@gmail.com>
2021-02-11 06:47:38 +0900

    feat: External interface에 POST, GET 추가

---
+++ app/drinks/external_interface/routers.py
@@ -0,0 +1,53 @@
+from typing import Optional, Union
+
+from container import Container
+from dependency_injector.wiring import Provide, inject
+from drinks.application.dtos import CreateDrinkInputDto, FindDrinksInputDto
+from drinks.application.service import DrinkApplicationService
+from drinks.external_interface.json_dto import (CreateDrinkJsonRequest,
+                                                GetDrinksJsonRequest,
+                                                GetDrinksJsonResponse)
+from fastapi import APIRouter, Depends
+from shared_kernel.external_interface.json_dto import FailedJsonResponse
+from starlette import status
+from starlette.responses import JSONResponse
+
+router = APIRouter(
+    prefix="/drinks",
+    tags=["drinks"],
+)
+
+
+@router.post("", status_code=status.HTTP_201_CREATED)
+@inject
+def create_drink(
+    request: CreateDrinkJsonRequest,
+    drink_application_service: DrinkApplicationService = Depends(Provide[Container.drink_application_service]),
+) -> Optional[JSONResponse]:
+    input_dto = CreateDrinkInputDto(
+        drink_id=request.drink_id,
+        drink_name=request.drink_name,
+        drink_image_url=request.drink_image_url,
+        drink_type=request.drink_type,
+    )
+    print(input_dto)
+    output_dto = drink_application_service.create_drink(input_dto=input_dto)
+    if not output_dto.status:
+        return FailedJsonResponse.build_by_output_dto(output_dto)
+
+
+@router.get("", status_code=status.HTTP_200_OK, response_model=GetDrinksJsonResponse)
+@inject
+def get_drinks(
+    request: GetDrinksJsonRequest,
+    drink_application_service: DrinkApplicationService = Depends(Provide[Container.drink_application_service]),
+) -> Union[GetDrinksJsonResponse, JSONResponse]:
+    input_dto = FindDrinksInputDto(
+        drink_type=request.drink_type,
+        filter_type=request.filter_type,
+        order=request.order,
+    )
+    output_dto = drink_application_service.find_drinks(input_dto=input_dto)
+    if not output_dto.status:
+        return FailedJsonResponse.build_by_output_dto(output_dto)
+    return GetDrinksJsonResponse.build_by_output_dto(output_dto)
