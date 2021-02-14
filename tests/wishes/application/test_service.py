533c945135c53c61e886c616c04f57ab280b153c
mayjunjuly0313 <tank6138@gmail.com>
2021-02-14 15:23:44 +0900

    test: wishes test_service에 drinks 관련 기능 추가

--- tests/wishes/application/test_service.py
+++ tests/wishes/application/test_service.py
@@ -1,5 +1,11 @@
 import pytest
+from drinks.application.dtos import CreateDrinkInputDto
+from drinks.application.service import DrinkApplicationService
+from drinks.domain.entities import Drink
+from drinks.domain.value_objects import DrinkRating, DrinkType
+from drinks.domain.value_objects import DrinkId as drinks_DrinkId
 
+from drinks.infra_structure.in_memory_repository import InMemoryDrinkRepository
 from wishes.application.dto import (
     CreateWishInputDto,
     CreateWishOutputDto,
@@ -10,7 +16,7 @@ from wishes.application.dto import (
 from wishes.application.service import WishApplicationService
 from wishes.domain.entities import Wish
 from wishes.domain.repository import QueryParam
-from wishes.domain.value_objects import UserId, DrinkId
+from wishes.domain.value_objects import DrinkId, UserId
 from wishes.infra_structure.in_memory_repository import InMemoryWishRepository
 
 
@@ -19,9 +25,19 @@ def wish_repository():
     return InMemoryWishRepository()
 
 
+@pytest.fixture
+def drink_repository():
+    return InMemoryDrinkRepository()
+
+
 @pytest.fixture(scope="function")
-def wish_application_service(wish_repository):
-    return WishApplicationService(wish_repository=wish_repository)
+def wish_application_service(wish_repository, drink_repository):
+    return WishApplicationService(wish_repository=wish_repository, drink_repository=drink_repository)
+
+
+@pytest.fixture(scope="function")
+def drink_application_service(drink_repository):
+    return DrinkApplicationService(drink_repository=drink_repository)
 
 
 def test_find_create_wish(wish_application_service, wish_repository):
@@ -65,18 +81,48 @@ def test_find_create_wish(wish_application_service, wish_repository):
     ]
 
 
-def test_create_wish(wish_application_service, wish_repository):
+def test_create_wish(wish_application_service, wish_repository, drink_application_service, drink_repository):
+    input_dto = CreateDrinkInputDto(
+        drink_id="335ca1a4-5175-5e41-8bac-40ffd840834c",
+        drink_name="Golden Ale",
+        drink_image_url="picture of golden ale",
+        drink_type=DrinkType.BEER,
+    )
+    drink_application_service.create_drink(input_dto)
+
     input_dto = CreateWishInputDto(user_id="heumsi", drink_id="335ca1a4-5175-5e41-8bac-40ffd840834c")
     output_dto = wish_application_service.create_wish(input_dto)
     assert output_dto.status is True
     assert output_dto == CreateWishOutputDto(
         user_id="heumsi", drink_id="335ca1a4-5175-5e41-8bac-40ffd840834c", created_at=output_dto.created_at
     )
+    # update num_of_wish field in drink entity
+    actual = drink_repository.find_all_simple()
+    expected = [
+        Drink(
+            id=drinks_DrinkId.from_str("335ca1a4-5175-5e41-8bac-40ffd840834c"),
+            name="Golden Ale",
+            image_url="picture of golden ale",
+            type=DrinkType.BEER,
+            avg_rating=DrinkRating(),
+            num_of_reviews=0,
+            num_of_wish=1,
+        )
+    ]
+    assert actual == expected
+
     wish = wish_repository.find(QueryParam(user_id="heumsi", drink_id="335ca1a4-5175-5e41-8bac-40ffd840834c"))
     assert wish is not None
 
 
-def test_delete_wish(wish_application_service, wish_repository):
+def test_delete_wish(wish_application_service, wish_repository, drink_application_service, drink_repository):
+    input_dto = CreateDrinkInputDto(
+        drink_id="335ca1a4-5175-5e41-8bac-40ffd840834c",
+        drink_name="Golden Ale",
+        drink_image_url="picture of golden ale",
+        drink_type=DrinkType.BEER,
+    )
+    drink_application_service.create_drink(input_dto)
     wish_repository.add(
         Wish(
             user_id=UserId(value="heumsi"),
