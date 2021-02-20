f7c02919e2aa4dd46bde57080a45af13bf0403f6
heumsi <heumsi@naver.com>
2021-02-20 23:25:42 +0900

    refactor: 변경된 Repository 에 맞춰 Service 도 수정

--- app/wishes/application/dto.py
+++ app/wishes/application/dto.py
@@ -12,6 +12,7 @@ class FindWishesInputDto(BaseModel):
 
 class FindWishesOutputDto(SuccessOutputDto):
     class Item(BaseModel):
+        id: str
         user_id: str
         drink_id: str
         created_at: float
@@ -25,14 +26,14 @@ class CreateWishInputDto(BaseModel):
 
 
 class CreateWishOutputDto(SuccessOutputDto):
+    id: str
     user_id: str
     drink_id: str
     created_at: float
 
 
 class DeleteWishInputDto(BaseModel):
-    user_id: str
-    drink_id: str
+    wish_id: str
 
 
 class DeleteWishOutputDto(SuccessOutputDto):
