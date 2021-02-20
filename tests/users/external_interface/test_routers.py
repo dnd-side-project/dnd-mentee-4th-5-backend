3e5ea0c814ad23f925d0e6276788b2ed63572841
heumsi <heumsi@naver.com>
2021-02-20 15:44:34 +0900

    test: test code 추가

--- tests/users/external_interface/test_routers.py
+++ tests/users/external_interface/test_routers.py
@@ -1,156 +1,124 @@
-from users.external_interface.json_dto import (CreateUserJsonRequest,
-                                               UpdateUserJsonRequest)
+from unittest import mock
+
+import pytest
+
+from shared_kernel.application.dtos import FailedOutputDto
+from users.application.dtos import FindUserOutputDto, CreateUserOutputDto, UpdateUserOutputDto, DeleteUserOutputDto
+from users.application.service import UserApplicationService
 
 
-def test_get_users(client):
-    # valid request but no resource
-    response = client.get("/users/heumsi")
-    assert response.status_code == 404
-    assert response.json() == {
-        "error_type": "Resource Not Found Error",
-        "message": "heumsi의 유저를 찾지 못했습니다.",
-    }
+@pytest.fixture(scope="function")
+def user_application_service_mock():
+    return mock.Mock(spec=UserApplicationService)
 
-    # valid request
-    response = client.post(
-        "/users", json={"user_id": "heumsi", "user_name": "heumsi", "password": "1234"}
+
+@pytest.fixture(scope="function")
+def auth_application_service_mock():
+    return mock.Mock()
+
+
+def test_get_users_success(user_application_service_mock, client, app):
+    user_application_service_mock.find_user.return_value = FindUserOutputDto(
+        user_id="heumsi", user_name="heumsi", description="", image_url=""
     )
-    assert response.status_code == 201
 
-    response = client.get("/users/heumsi")
+    with app.container.user_application_service.override(user_application_service_mock):
+        response = client.get("/users/heumsi")
     assert response.status_code == 200
+    assert response.json() == {"user_id": "heumsi", "user_name": "heumsi", "description": "", "image_url": ""}
+
+
+def test_get_users_fail(user_application_service_mock, client, app):
+    user_application_service_mock.find_user.return_value = FailedOutputDto.build_resource_not_found_error()
+
+    with app.container.user_application_service.override(user_application_service_mock):
+        response = client.get("/users/heumsi")
+    assert response.status_code == 404
     assert response.json() == {
-        "user_id": "heumsi",
-        "user_name": "heumsi",
-        "description": "",
-        "image_url": "",
+        "error_type": "Resource Not Found Error",
+        "message": "",
     }
 
 
-def test_post_users(client):
-    response = client.post(
-        "/users", json={"user_id": "heumsi", "user_name": "heumsi", "password": "1234"}
+def test_post_users_success(user_application_service_mock, client, app):
+    user_application_service_mock.create_user.return_value = CreateUserOutputDto(
+        user_id="heumsi", user_name="heumsi", description="", image_url=""
     )
-    assert response.status_code == 201
 
-    response = client.post(
-        "/users", json={"user_id": "heumsi", "user_name": "heumsi", "password": "1234"}
+    with app.container.user_application_service.override(user_application_service_mock):
+        response = client.post("/users", json={"user_id": "heumsi", "user_name": "heumsi", "password": "1234"})
+    assert response.status_code == 201
+    assert response.json() == {"user_id": "heumsi", "user_name": "heumsi", "description": "", "image_url": ""}
+
+
+def test_post_users_fail(user_application_service_mock, client, app):
+    user_application_service_mock.create_user.return_value = FailedOutputDto(
+        type="Resource Conflict Error", message="heumsi는 이미 존재하는 유저입니다."
     )
+    with app.container.user_application_service.override(user_application_service_mock):
+        response = client.post("/users", json={"user_id": "heumsi", "user_name": "heumsi", "password": "1234"})
     assert response.status_code == 409
-    assert response.json() == {
-        "error_type": "Resource Conflict Error",
-        "message": "heumsi는 이미 존재하는 유저입니다.",
-    }
+    assert response.json() == {"error_type": "Resource Conflict Error", "message": "heumsi는 이미 존재하는 유저입니다."}
 
 
-def test_put_users(client):
-    # wrong request without header
-    response = client.put(
-        "/users",
-        json=UpdateUserJsonRequest(
-            user_name="heumsi", description="시흠입니다.", password="1234"
-        ).dict(),
+def test_put_users_fail_with_not_exist_user(user_application_service_mock, client, app):
+    user_application_service_mock.update_user.return_value = FailedOutputDto(
+        type="Resource Not Found Error", message="heumsi의 유저를 찾지 못했습니다."
     )
-    assert response.status_code == 422
 
-    # wrong request with wrong header value
-    response = client.put(
-        "/users",
-        headers={"access-token": "wrong header value"},
-        json=UpdateUserJsonRequest(
-            user_name="heumsi", description="시흠입니다.", password="1234"
-        ).dict(),
-    )
-    assert response.status_code == 401
-    assert response.json() == {
-        "error_type": "Unauthorized Error",
-        "message": "올바른 access-token이 아닙니다.",
-    }
+    with app.container.user_application_service.override(user_application_service_mock):
+        response = client.put(
+            "/users",
+            headers={
+                "access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaGV1bXNpIn0.OuFWvZ07CwSzR1j7I-wxFHweVb6sB8_U2LezYL7nz3I"
+            },
+            json={"user_name": "heumsi", "description": "시흠입니다.", "password": "1234"},
+        )
+    assert response.status_code == 404
+    assert response.json() == {"error_type": "Resource Not Found Error", "message": "heumsi의 유저를 찾지 못했습니다."}
 
-    # valid request but no resource
-    response = client.put(
-        "/users",
-        headers={
-            "access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaGV1bXNpIn0.OuFWvZ07CwSzR1j7I-wxFHweVb6sB8_U2LezYL7nz3I"
-        },
-        json=UpdateUserJsonRequest(
-            user_name="heumsi", description="시흠입니다.", password="1234"
-        ).dict(),
+
+def test_put_users_success(user_application_service_mock, client, app):
+    user_application_service_mock.update_user.return_value = UpdateUserOutputDto()
+
+    with app.container.user_application_service.override(user_application_service_mock):
+        response = client.put(
+            "/users",
+            headers={
+                "access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaGV1bXNpIn0.OuFWvZ07CwSzR1j7I-wxFHweVb6sB8_U2LezYL7nz3I"
+            },
+            json={"user_name": "heumsi", "description": "Hi, I'm heumsi", "password": "1234"},
+        )
+    assert response.status_code == 204
+
+
+def test_delete_users_success(user_application_service_mock, client, app):
+    user_application_service_mock.delete_user.return_value = DeleteUserOutputDto()
+
+    with app.container.user_application_service.override(user_application_service_mock):
+        response = client.delete(
+            "/users",
+            headers={
+                "access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaGV1bXNpIn0.OuFWvZ07CwSzR1j7I-wxFHweVb6sB8_U2LezYL7nz3I"
+            },
+        )
+    assert response.status_code == 204
+
+
+def test_delete_users_fail(user_application_service_mock, client, app):
+    user_application_service_mock.delete_user.return_value = FailedOutputDto.build_resource_not_found_error(
+        message="heumsi의 유저를 찾지 못했습니다."
     )
+
+    with app.container.user_application_service.override(user_application_service_mock):
+        response = client.delete(
+            "/users",
+            headers={
+                "access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaGV1bXNpIn0.OuFWvZ07CwSzR1j7I-wxFHweVb6sB8_U2LezYL7nz3I"
+            },
+        )
     assert response.status_code == 404
     assert response.json() == {
         "error_type": "Resource Not Found Error",
         "message": "heumsi의 유저를 찾지 못했습니다.",
     }
-
-    # valid request
-    response = client.post(
-        "/users",
-        json=CreateUserJsonRequest(
-            user_id="heumsi", user_name="heumsi", password="1234"
-        ).dict(),
-    )
-    assert response.status_code == 201
-
-    response = client.put(
-        "/users",
-        headers={
-            "access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaGV1bXNpIn0.OuFWvZ07CwSzR1j7I-wxFHweVb6sB8_U2LezYL7nz3I"
-        },
-        json=UpdateUserJsonRequest(
-            user_name="siheum", description="시흠입니다.", password="1234"
-        ).dict(),
-    )
-    assert response.status_code == 204
-
-
-def test_delete_users(client):
-    # wrong request without header
-    response = client.delete(
-        "/users",
-    )
-    assert response.status_code == 422
-
-    # wrong request with wrong header value
-    response = client.delete(
-        "/users",
-        headers={"access-token": "wrong header value"},
-    )
-    assert response.status_code == 401
-    assert response.json() == {
-        "error_type": "Unauthorized Error",
-        "message": "올바른 access-token이 아닙니다.",
-    }
-
-    # valid request but no resource
-    response = client.delete(
-        "/users",
-        headers={
-            "access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaGV1bXNpIn0.OuFWvZ07CwSzR1j7I-wxFHweVb6sB8_U2LezYL7nz3I"
-        },
-        json=UpdateUserJsonRequest(
-            user_name="heumsi", description="시흠입니다.", password="1234"
-        ).dict(),
-    )
-    assert response.status_code == 404
-    assert response.json() == {
-        "error_type": "Resource Not Found Error",
-        "message": "heumsi의 유저를 찾지 못했습니다.",
-    }
-
-    # valid request
-    response = client.post(
-        "/users",
-        json=CreateUserJsonRequest(
-            user_id="heumsi", user_name="heumsi", password="1234"
-        ).dict(),
-    )
-    assert response.status_code == 201
-
-    response = client.delete(
-        "/users",
-        headers={
-            "access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaGV1bXNpIn0.OuFWvZ07CwSzR1j7I-wxFHweVb6sB8_U2LezYL7nz3I"
-        },
-    )
-    assert response.status_code == 204
---
+++ tests/users/infra_structure/test_orm_repository.py
@@ -0,0 +1,122 @@
+import pytest
+
+from shared_kernel.domain.exceptions import ResourceNotFoundError, ResourceAlreadyExistError
+from users.domain.entities import User
+from shared_kernel.domain.value_objects import UserId, UserName
+from users.infra_structure.orm_repository import OrmUserRepository
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup(database):
+    with database.session() as session:
+        session.execute("DELETE FROM user;")
+        session.execute(
+            "INSERT INTO user(id, name, description, password, image_url) VALUES "
+            "('heumsi', 'heumsi', 'hi, I am heumsi', '1234', ''), "
+            "('joon', 'joon', 'hello, I am joon', '4321', '')"
+        )
+        session.commit()
+
+
+@pytest.fixture(scope="function")
+def orm_user_repository(database):
+    return OrmUserRepository(session_factory=database.session)
+
+
+def test_find_all(orm_user_repository):
+    actual = orm_user_repository.find_all()
+    expected = [
+        User(
+            id=UserId(value="heumsi"),
+            name=UserName(value="heumsi"),
+            description="hi, I am heumsi",
+            password="1234",
+            image_url="",
+        ),
+        User(
+            id=UserId(value="joon"),
+            name=UserName(value="joon"),
+            description="hello, I am joon",
+            password="4321",
+            image_url="",
+        ),
+    ]
+    assert actual == expected
+
+
+def test_find_by_user_id(orm_user_repository):
+    actual = orm_user_repository.find_by_user_id(user_id=UserId(value="heumsi"))
+    expected = User(
+        id=UserId(value="heumsi"),
+        name=UserName(value="heumsi"),
+        description="hi, I am heumsi",
+        password="1234",
+        image_url="",
+    )
+    assert actual == expected
+
+    with pytest.raises(ResourceNotFoundError):
+        orm_user_repository.find_by_user_id(user_id=UserId(value="not exist user"))
+
+
+def test_add(orm_user_repository):
+    orm_user_repository.add(user=User(id=UserId(value="siheum"), name=UserName(value="siheum"), password="1234"))
+    actual = orm_user_repository.find_by_user_id(user_id=UserId(value="siheum"))
+    expected = User(
+        id=UserId(value="siheum"),
+        name=UserName(value="siheum"),
+        description="",
+        password="1234",
+        image_url="",
+    )
+    assert actual == expected
+
+    with pytest.raises(ResourceAlreadyExistError):
+        orm_user_repository.add(
+            user=User(
+                id=UserId(value="siheum"),
+                name=UserName(value="siheum"),
+                password="1234",
+            )
+        )
+
+
+def test_update(orm_user_repository):
+    orm_user_repository.update(
+        user=User(
+            id=UserId(value="heumsi"),
+            name=UserName(value="heumsi jeon"),
+            description="Hi, I'm heumsi jeon!",
+            password="1234",
+            image_url="",
+        )
+    )
+    actual = orm_user_repository.find_by_user_id(user_id=UserId(value="heumsi"))
+    expected = User(
+        id=UserId(value="heumsi"),
+        name=UserName(value="heumsi jeon"),
+        description="Hi, I'm heumsi jeon!",
+        password="1234",
+        image_url="",
+    )
+    assert actual == expected
+
+    with pytest.raises(ResourceNotFoundError):
+        orm_user_repository.update(
+            user=User(
+                id=UserId(value="not exist user"),
+                name=UserName(value="heumsi jeon"),
+                description="Hi, I'm heumsi jeon!",
+                password="1234",
+                image_url="",
+            )
+        )
+
+
+def test_delete(orm_user_repository):
+    with pytest.raises(ResourceNotFoundError):
+        orm_user_repository.find_by_user_id(user_id=UserId(value="not exist user"))
+
+    orm_user_repository.delete_by_user_id(UserId(value="heumsi"))
+    with pytest.raises(ResourceNotFoundError):
+        orm_user_repository.find_by_user_id(user_id=UserId(value="heumsi"))
