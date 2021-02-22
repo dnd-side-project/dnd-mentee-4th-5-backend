b5e669801dc67c3f3b8da0c71d25cdd1d2307b1f
heumsi <heumsi@naver.com>
2021-02-22 23:18:32 +0900

    refactor: UUID 를 이용하여 ORM 컬럼 타입 구성

--- app/wishes/domain/value_objects.py
+++ app/wishes/domain/value_objects.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 
 
 class WishId(BaseModel):
-    __root__: uuid.UUID = Field(alias="value")
+    value: uuid.UUID
 
     @classmethod
     def build(cls, user_id: str, drink_id: str) -> "WishId":
@@ -14,13 +14,9 @@ class WishId(BaseModel):
     def from_str(cls, wish_id: str) -> "WishId":
         return cls(value=uuid.UUID(wish_id))
 
-    @classmethod
-    def from_bytes(cls, bytes_: bytes):
-        return cls(value=uuid.UUID(bytes=bytes_))
-
     @property
-    def bytes(self):
-        return self.__root__.bytes
+    def uuid(self) -> uuid.UUID:
+        return self.value
 
     def __str__(self):
-        return str(self.__root__)
+        return str(self.value)
