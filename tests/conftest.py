1da4971d6f861059922b667d9ea617f30bf5b5dd
heumsi <heumsi@naver.com>
2021-02-26 01:14:27 +0900

    test: 환경변수에 TEST_DB_URL 추가

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from starlette.testclient import TestClient
 
@@ -7,7 +9,7 @@ from shared_kernel.infra_structure.database import Database
 # fixtures for database (testing ORM Repository)
 @pytest.fixture(scope="session")
 def database():
-    database = Database(db_url=f"postgresql://root:1234@localhost:5432/coholy_test")
+    database = Database(db_url=os.environ["TEST_DB_URL"])
     database.create_database()
     return database
 
